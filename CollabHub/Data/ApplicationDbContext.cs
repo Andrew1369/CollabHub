00d5b08eb01f350388f674701e5e5a8298c6169b
Andrew1369 <157983860+Andrew1369@users.noreply.github.com>
2025-11-02 01:17:46 +0200

    feat(db): add ApplicationDbContext and sqlite connection

---
+++ CollabHub/Data/ApplicationDbContext.cs
@@ -0,0 +1,42 @@
+﻿using CollabHub.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CollabHub.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options) { }
+
+
+        public DbSet<Organization> Organizations => Set<Organization>();
+        public DbSet<Venue> Venues => Set<Venue>();
+        public DbSet<Event> Events => Set<Event>();
+        public DbSet<Asset> Assets => Set<Asset>();
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Venue>()
+            .HasOne(v => v.Organization)
+            .WithMany(o => o.Venues)
+            .HasForeignKey(v => v.OrganizationId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+
+            modelBuilder.Entity<Event>()
+            .HasOne(e => e.Venue)
+            .WithMany(v => v.Events)
+            .HasForeignKey(e => e.VenueId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+
+            modelBuilder.Entity<Asset>()
+            .HasOne(a => a.Event)
+            .WithMany(e => e.Assets)
+            .HasForeignKey(a => a.EventId)
+            .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
